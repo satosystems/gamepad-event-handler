55b2672b029b6230c97ce8a354d656f7f6815b4b
Satoshi Ogata <satosystems@gmail.com>
2022-09-23 20:26:30 +0900

    Update implements

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,9 @@ module Lib
   ) where
 
 import           Control.Monad                  ( unless )
+import           Data.Foldable                  ( forM_ )
+import           Data.Functor                   ( (<&>) )
+import qualified Data.Vector                   as V
 import           SDL                            ( ($=)
                                                 , ControllerButtonEventData
                                                   ( ControllerButtonEventData
@@ -13,10 +16,14 @@ import           SDL                            ( ($=)
                                                 , Event(eventPayload)
                                                 , EventPayload
                                                   ( ControllerButtonEvent
+                                                  , JoyAxisEvent
                                                   , JoyButtonEvent
                                                   , KeyboardEvent
                                                   )
                                                 , InputMotion(Pressed)
+                                                , JoyAxisEventData
+                                                  ( JoyAxisEventData
+                                                  )
                                                 , JoyButtonEventData
                                                   ( JoyButtonEventData
                                                   )
@@ -29,13 +36,17 @@ import           SDL                            ( ($=)
                                                 , Keysym(Keysym, keysymKeycode)
                                                 , Renderer
                                                 , V4(V4)
+                                                , availableJoysticks
                                                 , clear
+                                                , closeJoystick
                                                 , createRenderer
                                                 , createWindow
                                                 , defaultRenderer
                                                 , defaultWindow
                                                 , destroyWindow
                                                 , initializeAll
+                                                , numJoysticks
+                                                , openJoystick
                                                 , pollEvents
                                                 , present
                                                 , rendererDrawColor
@@ -44,9 +55,20 @@ import           SDL                            ( ($=)
 start :: IO ()
 start = do
   initializeAll
-  window   <- createWindow "My SDL Application" defaultWindow
-  renderer <- createRenderer window (-1) defaultRenderer
+  putStrLn "Press Q when quit"
+  joysticks <- numJoysticks
+  putStr "numJoysticks: "
+  print joysticks
+  availables <- availableJoysticks
+  putStr "availableJoysticks: "
+  print availables
+  window    <- createWindow "My SDL Application" defaultWindow
+  renderer  <- createRenderer window (-1) defaultRenderer
+  mJoystick <- if not . null $ availables
+    then openJoystick (V.head availables) Data.Functor.<&> Just
+    else return Nothing
   appLoop renderer
+  forM_ mJoystick closeJoystick
   destroyWindow window
 
 appLoop :: Renderer -> IO ()
@@ -70,6 +92,10 @@ appLoop renderer = do
         putStrLn "JoyButtonEvent"
         print button
         print state
+      JoyAxisEvent (JoyAxisEventData _ axis value) -> do
+        putStrLn "JoyAxisEvent"
+        print axis
+        print value
       KeyboardEvent (KeyboardEventData _ _ _ (Keysym _ keycode _)) -> do
         putStrLn "KeyboardEvent"
         print keycode
